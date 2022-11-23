c4d2f75cfab0c798f07fd5ca197d745b86be4ec0
PushkarGaur10 <gaurpushkar10@gmail.com>
2022-11-23 21:29:41 +0530

    Updates

---
+++ precache-manifest.76933e4055632c85a0f687579b0f0b46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6b94e059c48cfd3b5c5c6772d054b7",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "456c11ad6e5d14689603",
+    "url": "/toDoList/static/js/2.473fd0d5.chunk.js"
+  },
+  {
+    "revision": "dea9c992bfe68bebb4b5",
+    "url": "/toDoList/static/js/main.a204493f.chunk.js"
+  },
+  {
+    "revision": "1563ba0b5b94253e0356",
+    "url": "/toDoList/static/js/runtime-main.cf9cf6f2.js"
+  }
+]);
